5ae0d9e279a0390f177c166bbefc2a86d3483b98
Ethan Swan <ethanpswan@gmail.com>
2023-03-06 16:16:36 -0500

    Add repo name to slides.

--- uc_python/build.py
+++ uc_python/build.py
@@ -2,11 +2,16 @@ import os
 from subprocess import run
 from pathlib import Path
 
+from .repos import repos
+
 folder = Path("repos").absolute()
 repo_paths = [f for f in folder.glob("*") if f.is_dir()]
 
 for repo in repo_paths:
     os.chdir(folder / repo)
+    # Figure out which Repo this corresponds to.
+    repo_name = [r for r in repos if repo.name in r.url][0].name
+
     if not (repo / "Makefile").exists():
         print("no Makefile, skipping")
         continue
@@ -18,7 +23,7 @@ for repo in repo_paths:
         print("generating index.html")
         index = slides_dir / "index.html"
         with open(index, "at") as f:
-            f.write("<h1>Slides</h1>\n")
+            f.write(f"<h1>{repo_name}</h1>\n")
             f.write("<ul>\n")
             slide_files = sorted(slides_dir.glob("*.slides.html"))
             for slide_file in slide_files:
