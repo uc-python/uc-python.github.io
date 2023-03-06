95688d63a0a45e63c3d63e4e8fb78df235c7d0bc
Ethan Swan <ethanpswan@gmail.com>
2023-03-06 15:18:15 -0500

    Fix build script.

--- uc_python/build.py
+++ uc_python/build.py
@@ -2,11 +2,15 @@ import os
 from subprocess import run
 from pathlib import Path
 
-folder = Path("repos")
+folder = Path("repos").absolute()
+repo_paths = [f for f in folder.glob("*") if f.is_dir()]
 
-for repo in folder.glob("*"):
-    os.chdir(repo)
+for repo in repo_paths:
+    os.chdir(folder / repo)
+    if not (repo / "Makefile").exists():
+        print("no Makefile, skipping")
+        continue
     print(f"building slides for {repo.name}...")
     run(["make", "slides"], check=True)
     print("done")
-    os.chdir("..")
+    print(os.getcwd())
--- uc_python/collate.py
+++ uc_python/collate.py
@@ -6,7 +6,7 @@ site.mkdir(exist_ok=True)
 
 # Build an index as we go
 index = site / "index.html"
-index.touch()
+index.write_text("<h1>UC Python Slides</h1>")
 for repo in folder.glob("*"):
     (repo / "slides").rename(site / repo.name)
     index.write_text(f'<a href="/{repo.name}">{repo.name}</a>')
